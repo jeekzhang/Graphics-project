69b0df1acbb8c5d4a8339920134bddf23b35f6c4
张计科 <78347874+jeek-zhang@users.noreply.github.com>
2023-05-02 13:50:47 +0800

    add -jitter -filter code

--- src/Material.cpp
+++ src/Material.cpp
@@ -6,7 +6,7 @@ Vector3f Material::shade(const Ray &ray,
     const Vector3f &lightIntensity)
 {
     // TODO implement Diffuse and Specular phong terms
-    //这里各向量与图中的对应我还不好说，我估计dirToLight是L，ray.getDirection()是-E
+    //dirToLight是L，ray.getDirection()是-E
     float clamp_LN = Vector3f::dot(dirToLight, hit.getNormal());
     if (clamp_LN < 0)
         clamp_LN = 0;
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -18,12 +18,24 @@ Renderer::Renderer(const ArgParser &args) :
 void
 Renderer::Render()
 {
+    int k = 1;
+    if (_args.filter)
+        k = 3;
+    int count = 1;
+    if (_args.jitter)
+        count = 16;
     int w = _args.width;
     int h = _args.height;
 
     Image image(w, h);
     Image nimage(w, h);
     Image dimage(w, h);
+
+    w *= k;
+    h *= k;
+    Image kimage(w, h);
+    Image knimage(w, h);
+    Image kdimage(w, h);
 
     // loop through all the pixels in the image
     // generate all the samples
@@ -32,37 +44,86 @@ Renderer::Render()
     // It also write to the color, normal, and depth images.
     // You should understand what this code does.
     Camera* cam = _scene.getCamera();
-    for (int y = 0; y < h; ++y) {
-        float ndcy = 2 * (y / (h - 1.0f)) - 1.0f;
-        for (int x = 0; x < w; ++x) {
-            float ndcx = 2 * (x / (w - 1.0f)) - 1.0f;
+    float h_base = 2 / (h - 1.0f);
+    float w_base = 2 / (w - 1.0f);
+    for (int y = 0; y < h; ++y)
+    {
+        float ndcy = y * h_base - 1.0f;
+        for (int x = 0; x < w; ++x)
+        {
+            float ndcx = x * w_base - 1.0f;
             // Use PerspectiveCamera to generate a ray.
             // You should understand what generateRay() does.
-            int count = 1;
-            if (_args.jitter)
-                count = 16;
+
             for (int i = 0; i < count; i++)
             {
                 srand(time(NULL));
                 double random_x = (double)rand() / RAND_MAX;
                 double random_y = (double)rand() / RAND_MAX;
-                ndcx += random_x;
-                ndcy += random_y;
+                if (i == 0)
+                    random_x = random_y = 0.0;
+                random_x = ndcx + random_x * w_base;
+                random_y = ndcy + random_y * h_base;
 
-                Ray r = cam->generateRay(Vector2f(ndcx, ndcy));
+                Ray r = cam->generateRay(Vector2f(random_x, random_y));
 
                 Hit h;
                 Vector3f color = traceRay(r, cam->getTMin(), _args.bounces, h);
 
-                image.setPixel(x, y, color);
-                nimage.setPixel(x, y, (h.getNormal() + 1.0f) / 2.0f);
+                kimage.setPixel(x, y, color);
+                knimage.setPixel(x, y, (h.getNormal() + 1.0f) / 2.0f);
                 float range = (_args.depth_max - _args.depth_min);
                 if (range)
                 {
-                    dimage.setPixel(x, y, Vector3f((h.t - _args.depth_min) / range));
+                    kdimage.setPixel(x, y, Vector3f((h.t - _args.depth_min) / range));
                 }
             }
         }
+    }
+    //只处理样例k=3的情况即可
+    if (k == 3)
+        for (int y = 0; y < h / k; ++y)
+            for (int x = 0; x < w / k; ++x)
+            {
+                {
+                    int center_x = x * k + 1;
+                    int center_y = y * k + 1;
+                    Vector3f color(0, 0, 0);
+                    Vector3f normal(0, 0, 0);
+                    Vector3f depth(0, 0, 0);
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            if (abs(i) + abs(j) == 2)
+                            {
+                                color += kimage.getPixel(center_x + i, center_y + j) / 16;
+                                normal += knimage.getPixel(center_x + i, center_y + j) / 16;
+                                depth += kdimage.getPixel(center_x + i, center_y + j) / 16;
+                            }
+
+                            if (abs(i) + abs(j) == 1)
+                            {
+                                color += kimage.getPixel(center_x + i, center_y + j) / 8;
+                                normal += knimage.getPixel(center_x + i, center_y + j) / 8;
+                                depth += kdimage.getPixel(center_x + i, center_y + j) / 8;
+                            }
+                            else
+                            {
+                                color += kimage.getPixel(center_x + i, center_y + j) / 4;
+                                normal += knimage.getPixel(center_x + i, center_y + j) / 4;
+                                depth += kdimage.getPixel(center_x + i, center_y + j) / 4;
+                            }
+                        }
+                        image.setPixel(x,y,color);
+                        nimage.setPixel(x,y,normal);
+                        dimage.setPixel(x,y,depth);
+                }
+            }
+    else if (k == 1)
+    {
+        image = kimage;
+        dimage = kdimage;
+        nimage = knimage;
     }
     // END SOLN
 
@@ -106,10 +167,10 @@ Renderer::traceRay(const Ray &r,
             if (_args.shadows)
             {
                 // 以光打到的点作为光源，计算阴影覆盖的区域
-                Vector3f shadowRayOrigin = r.pointAtParameter(h.getT()) + 0.05 * dirToLight;
+                Vector3f shadowRayOrigin = r.pointAtParameter(h.getT()) + 0.01 * dirToLight;
                 Ray shadowRay(shadowRayOrigin, dirToLight);
                 Hit shadowHit = Hit();
-                Vector3f shadowTrace = traceRay(shadowRay, 0, 0, shadowHit);
+                Vector3f shadowTrace = traceRay(shadowRay, 0.0f, 0.0f, shadowHit);
                 // 阴影是否与物体相交
                 bool is_shadowIntersectedSth = shadowHit.getT() < std::numeric_limits<float>::max();
                 // 阴影与该物体相交的区域
